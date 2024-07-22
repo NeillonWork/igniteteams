93ca386c374c390d97162084b4a6f85649a3f937
Admin <neillonwork@gmail.com>
2024-07-22 14:05:09 -0300

    correçoes do babel module-resolver

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    //babel-plugin-module-resolver
     plugins: [
-      "module-resolver",
-      {
-        root: ["./src"],
-        alias: {
-          "@assets": "./src/assets",
-          "@components": "./src/components",
-          "@routes": "./src/routes",
-          "@screans": "./src/screans",
-          "@theme": "./src/theme",
-          "@utils": "./src/utils",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@theme": "./src/theme",
+            "@utils": "./src/utils",
+          },
         },
-      },
+      ],
     ],
   };
 };
