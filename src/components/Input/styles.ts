2b3fd8abbb7eaf89f11d73132ca5843cf9e5f7e3
Admin <neillonwork@gmail.com>
2024-07-27 19:46:27 -0300

    Ajustado navegação, e add SafeAreaView as paginas

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -7,13 +7,13 @@ export const Container = styled(TextInput)`
   min-height: 56px;
   max-height: 56px;
 
+  border-radius: 6px;
+  padding: 16px;
+
   ${({ theme }) => css`
     background-color: ${theme.COLORS.GRAY_700};
     color: ${theme.COLORS.WHITE};
     font-family: ${theme.FONT_FAMILY.REGULAR};
     font-size: ${theme.FONT_SIZE.MD}px;
   `};
-  
-  border-radius: 6px;
-  padding: 16px;
 `;
--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -3,8 +3,16 @@ import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroups() {
+const navigation = useNavigation();
+
+
+function handleNew(){
+navigation.navigate("players", {group: "Rocket"})
+}
+
   return (
     <Container>
       <Header showBackButton />
@@ -14,10 +22,10 @@ export function NewGroups() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Input
+        <Input 
           placeholder="Nome da turma"
         />
-        <Button title="Criar" style={{marginTop:20}}/>
+        <Button title="Criar" style={{marginTop:20}} onPress={handleNew} />
       </Content>
     </Container>
   );
