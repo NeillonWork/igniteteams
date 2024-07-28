6c720d33d6e90935a4dfef16a29a1b1e40174241
Admin <neillonwork@gmail.com>
2024-07-27 21:49:18 -0300

    removido glitch ao trocar de pagina

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
+import { NavigationContainer, useTheme } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
+import theme from "src/theme";
+
 
 export function Routes() {
+
   return (
+    <View style={{flex:1, backgroundColor: theme.COLORS.GRAY_600}}>
     <NavigationContainer >
       <AppRoutes />
     </NavigationContainer>
+    </View>
   );
 }
